b066b950bedac30078c3ba878dbbf19d294c4e29
Mara <edimara.rosa@hotmail.com>
2022-03-20 16:45:22 -0300

    first commit

---
+++ app/src/main/java/com/estudos/signupsignin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.estudos.signupsignin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
