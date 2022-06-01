2bfd818d45edcc1fcfb72bfa96e8aa6ec49689c7
Mara <edimara.rosa@hotmail.com>
2022-05-31 23:20:13 -0300

    ajuste

--- app/src/main/java/com/estudos/signupsignin/signup/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/estudos/signupsignin/signup/viewmodel/SignUpViewModel.kt
@@ -9,4 +9,65 @@ class SignUpViewModel : ViewModel() {
     val viewStateLiveData = MutableLiveData<SignUpViewState>()
     val commandLiveData = SingleLiveEvent<SignUpCommand>()
 
+    fun verifyInputValues(userValues: SignUpValues) {
+        val isEmptyFields = verifyEmptyFields(userValues)
+        val isAValidEmail = verifiyEmail(userValues.isAValidEmail)
+        val isAValidPassword = verifyPassword(
+            userPassword = userValues.password,
+            userConfirmPassword = userValues.confirmPassword
+        )
+        commandLiveData.value =
+            SignUpCommand.ChangeButtonState(
+                isEmptyFields
+                        && isAValidEmail
+                        && isAValidPassword
+            )
+    }
+
+    private fun verifyEmptyFields(values: SignUpValues): Boolean {
+        return values.name.isNotEmpty() && values.lastName.isNotEmpty()
+                && values.email.isNotEmpty() && values.phoneNumber.isNotEmpty()
+                && values.password.isNotEmpty() && values.confirmPassword.isNotEmpty()
+    }
+
+    private fun verifiyEmail(isAValidEmail: Boolean): Boolean {
+        return if (isAValidEmail) {
+            true
+        } else {
+            commandLiveData.value =
+                SignUpCommand.SendInvalidEmailMessage(R.string.generic_email_error)
+            false
+        }
+    }
+
+    private fun verifyPassword(userPassword: String, userConfirmPassword: String): Boolean {
+        return (userPassword.length >= MINIMUM_PASSOWORD_LENGTH && userPassword == userConfirmPassword)
+    }
+
+    fun onRegisterClick(request: RegisterUserInfoRequest) {
+        disposable.add(
+            interactor.sendUserInfo(request)
+                .subscribeOn(scheduler)
+                .observeOn(androidScheduler)
+                .doOnSubscribe { viewStateLiveData.value = SignUpViewState.Loading }
+                .subscribe(
+                    {
+                        viewStateLiveData.value = SignUpViewState.Success
+                    },
+                    {
+                        viewStateLiveData.value = SignUpViewState.Error
+                    }
+                )
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
+    private companion object {
+        private const val MINIMUM_PASSOWORD_LENGTH = 8
+    }
+
 }
